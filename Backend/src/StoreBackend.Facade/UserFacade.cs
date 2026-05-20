51f49f18a692c18c4f53cf38308f2a6f053edf0c
BrianPorras <brianporras6019@gmail.com>
2026-05-19 18:46:40 -0600

    Refactor some variables names

--- Backend/src/StoreBackend.Facade/UserFacade.cs
+++ Backend/src/StoreBackend.Facade/UserFacade.cs
@@ -7,38 +7,38 @@ namespace StoreBackend.Facade;
 
 public class UserFacade : IUserFacade
 {
-    private readonly IUserService userService;
-    private readonly AppDbContext context;
+    private readonly IUserService _userService;
+    private readonly AppDbContext _context;
 
     public UserFacade(IUserService userService, AppDbContext context)
     {
-        this.userService = userService;
-        this.context = context;
+        _userService = userService;
+        _context = context;
     }
 
     public async Task<UserDto> LoginAsync(LoginUserDto loginDto)
     {
-        var entity = await userService.LoginAsync(loginDto);
+        var entity = await _userService.LoginAsync(loginDto);
         return UserMapper.ToDto(entity);
     }
 
     public async Task<UserDto> CreateAsync(CreateUserDto userDto)
     {
-        var entity = await userService.CreateAsync(userDto);
-        await context.SaveChangesAsync();
+        var entity = await _userService.CreateAsync(userDto);
+        await _context.SaveChangesAsync();
         return UserMapper.ToDto(entity);
     }
 
     public async Task<UserDto> UpdateAsync(Guid resourceId, UpdateUserDto userDto)
     {
-        var entity = await userService.UpdateAsync(resourceId, userDto);
-        await context.SaveChangesAsync();
+        var entity = await _userService.UpdateAsync(resourceId, userDto);
+        await _context.SaveChangesAsync();
         return UserMapper.ToDto(entity);
     }
 
     public async Task DeleteAsync(Guid resourceId, string password)
     {
-        await userService.DeleteAsync(resourceId, password);
-        await context.SaveChangesAsync();
+        await _userService.DeleteAsync(resourceId, password);
+        await _context.SaveChangesAsync();
     }
 }
